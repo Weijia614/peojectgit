5efa0c6ba7f8586c77c1a9f0cf291a939e1dda6a
Weijia <weijia132@hotmail.com>
2025-02-09 14:26:32 +0100

    second commit

---
+++ peoject.py
@@ -0,0 +1,7 @@
+name = input('what is your name?')
+age = int(input('how old are you?'))
+age_5 = age +5
+age_10 = age +10
+print(f'Hello,{name}!')
+print(f'In 5 years, you will be {age_5} years old')
+print(f'In 10 years, you will be {age_10}')
